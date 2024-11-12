96ccc005406e477f5a9178c643431b85d137fa7a
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2024-11-12 14:26:21 +0600

    Bootsrap has been done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <>
-      <h1>This is Job Task Of Softnio</h1>
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
+      <button type="button" className="btn btn-secondary">
+        Secondary
+      </button>
+      <button type="button" className="btn btn-success">
+        Success
+      </button>
     </>
   )
 }
